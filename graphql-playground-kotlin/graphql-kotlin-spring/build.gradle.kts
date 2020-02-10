fff7a2bee7e9bbf864ace32fba525181775d18a7
Anddd7 <liaoad_space@sina.com>
2020-02-10 17:47:53 +0800

    add graphql kotlin dependencies into

--- graphql-playground-kotlin/graphql-kotlin-spring/build.gradle.kts
+++ graphql-playground-kotlin/graphql-kotlin-spring/build.gradle.kts
@@ -1,6 +1,25 @@
-dependencies {
-  implementation("com.graphql-java:graphql-java:14.0")
+plugins {
+  kotlin("plugin.spring")
 
-  implementation("com.expediagroup:graphql-kotlin-schema-generator:1.4.2")
-//  implementation("com.expediagroup:graphql-kotlin-schema-generator:2.0.0-RC4.3")
+  id("org.springframework.boot")
+  id("io.spring.dependency-management")
+}
+
+dependencies {
+  /* graphql */
+  implementation("com.expediagroup:graphql-kotlin-schema-generator:2.0.0-RC6")
+
+  /* webflux x reactor */
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+  testImplementation("org.springframework.boot:spring-boot-starter-test") {
+    exclude(module = "junit")
+    exclude(group = "org.mockito")
+    exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+  }
+  testImplementation("com.ninja-squad:springmockk:1.1.3")
+  testImplementation("io.projectreactor:reactor-test")
+
+  /* kotlin coroutines x reactor */
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 }
