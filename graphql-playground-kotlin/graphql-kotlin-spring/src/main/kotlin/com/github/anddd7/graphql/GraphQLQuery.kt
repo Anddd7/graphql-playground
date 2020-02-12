f180ac52f33950ddd01acc75ed777aa80362c1e3
Anddd7 <liaoad_space@sina.com>
2020-02-12 15:02:18 +0800

    nested query

--- graphql-playground-kotlin/graphql-kotlin-spring/src/main/kotlin/com/github/anddd7/graphql/GraphQLQuery.kt
+++ graphql-playground-kotlin/graphql-kotlin-spring/src/main/kotlin/com/github/anddd7/graphql/GraphQLQuery.kt
@@ -1,15 +1,16 @@
 package com.github.anddd7.graphql
 
 import com.expediagroup.graphql.spring.operations.Query
-import com.github.anddd7.entity.Book
+import com.github.anddd7.entity.AuthorRepository
 import com.github.anddd7.entity.BookRepository
 import com.github.anddd7.graphql.dto.toDTO
 import org.springframework.stereotype.Component
 
 @Component
 class GraphQLQuery(
-    private val bookRepository: BookRepository
+    private val bookRepository: BookRepository,
+    private val authorRepository: AuthorRepository
 ) : Query {
-  fun bookById(id: Int) = bookRepository.findById(id).toDTO()
-  fun books() = bookRepository.findAll().map(Book::toDTO)
+  fun bookById(id: Int) = bookRepository.findById(id).toDTO(authorRepository)
+  fun books() = bookRepository.findAll().map { it.toDTO(authorRepository) }
 }
--- graphql-playground-kotlin/graphql-kotlin-spring/src/main/kotlin/com/github/anddd7/graphql/dto/BookDTO.kt
+++ graphql-playground-kotlin/graphql-kotlin-spring/src/main/kotlin/com/github/anddd7/graphql/dto/BookDTO.kt
@@ -1,5 +1,6 @@
 package com.github.anddd7.graphql.dto
 
+import com.github.anddd7.entity.AuthorRepository
 import com.github.anddd7.entity.Book
 import com.github.anddd7.entity.Company
 
@@ -7,13 +8,25 @@ data class BookDTO(
     val id: Int = 0,
     val name: String,
     val pageCount: Int,
-    val company: CompanyDTO
-)
+    val company: CompanyDTO,
+    private val authorId: Int,
+    private val authorRepository: AuthorRepository
+) {
+  fun author() = authorRepository.findById(authorId).toDTO()
+}
 
 data class CompanyDTO(
     val name: String,
     val address: String
 )
 
-fun Book.toDTO() = BookDTO(id, name, pageCount, company.toDTO())
+fun Book.toDTO(authorRepository: AuthorRepository) = BookDTO(
+    id,
+    name,
+    pageCount,
+    company.toDTO(),
+    authorId,
+    authorRepository
+)
+
 fun Company.toDTO() = CompanyDTO(name, address)
