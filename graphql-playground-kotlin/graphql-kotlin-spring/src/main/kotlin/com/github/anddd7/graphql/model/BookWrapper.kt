7ced76dde2e9cfccccd42127b538be594b1f6e65
Anddd7 <liaoad_space@sina.com>
2020-02-13 16:43:31 +0800

    remove duplicate data classes

--- graphql-playground-kotlin/graphql-kotlin-spring/src/main/kotlin/com/github/anddd7/graphql/model/BookWrapper.kt
+++ graphql-playground-kotlin/graphql-kotlin-spring/src/main/kotlin/com/github/anddd7/graphql/model/BookWrapper.kt
@@ -1,31 +1,22 @@
 package com.github.anddd7.graphql.model
 
+import com.github.anddd7.persistence.AuthorPO
+import com.github.anddd7.persistence.CompanyPO
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter.ofPattern
 
-data class Book(
+data class BookWrapper(
     val id: Int = 0,
     val name: String,
     val pageCount: Int,
     private val authorId: Int,
     private val editorId: Int,
-    val company: Company,
+    val company: CompanyPO,
     private val publishedAt: LocalDateTime,
-    private val authorById: suspend (Int) -> Author
+    private val authorById: suspend (Int) -> AuthorPO
 ) {
   suspend fun author() = authorById(authorId)
   suspend fun editor() = authorById(editorId)
   fun publishedAt(pattern: String?): String =
       ofPattern(pattern ?: "dd, MMM, yyyy").let(publishedAt::format)
 }
-
-data class Company(
-    val name: String,
-    val address: String
-)
-
-data class Author(
-    val id: Int = 0,
-    val firstName: String,
-    val lastName: String
-)
